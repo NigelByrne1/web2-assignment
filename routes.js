81e2e4349be893d0361ef37dcb9caca27c404d3a
Nigel <nigelbyrnedesign@gmail.com>
2024-07-23 14:11:13 +0100

    Basic Branding Added, Clickable Stations & Station Controller

--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
+import { stationController } from "./controllers/station-controller.js";
+import { stat } from "fs";
 
 export const router = express.Router();
 
@@ -8,3 +10,4 @@ router.get("/", dashboardController.index);
 router.get("/dashboard", dashboardController.index);
 router.post("/dashboard/addstation", dashboardController.addStation);
 router.get("/about", aboutController.index);
+router.get("/station/:id", stationController.index);
