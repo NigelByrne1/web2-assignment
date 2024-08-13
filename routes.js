f1af9dcfccf1fa8bd7edd7cef20110d51dad456c
Nigel <nigelbyrnedesign@gmail.com>
2024-08-13 15:40:20 +0100

    Edit/Update Report Implemented

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
 import { stationController } from "./controllers/station-controller.js";
+import { reportController } from "./controllers/report-controller.js";
 
 export const router = express.Router();
 
@@ -13,3 +14,6 @@ router.get("/station/:id", stationController.index);
 router.get("/dashboard/deletestation/:id", dashboardController.deleteStation);
 router.post("/station/:id/addreport", stationController.addReport);
 router.get("/station/:stationid/deletereport/:reportid", stationController.deleteReport);
+
+router.get("/station/:stationid/editreport/:reportid", reportController.index);
+router.post("/station/:stationid/updatereport/:reportid", reportController.update);
